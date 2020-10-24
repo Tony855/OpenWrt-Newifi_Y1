c6d5b2ea0b8b75671cfdb0ce5de990c8d1e97a7d
Tony855 <58850110+Tony855@users.noreply.github.com>
2020-10-24 21:50:24 +0700

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,5 +11,5 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.33.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.28.1/g' package/base-files/files/bin/config_generate
 # sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-argon/root/etc/uci-defaults/30_luci-theme-argon
