1e3b7f1d137c5bc18705f029c7faa906d68c68d0
Tony855 <58850110+Tony855@users.noreply.github.com>
2020-10-26 04:54:45 +0700

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,7 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-git clone https://github.com/coolsnowwolf/lede/tree/master/package/lean package/lean
+# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+# git clone https://github.com/coolsnowwolf/lede/tree/master/package/lean package/lean
 git clone https://github.com/kuoruan/openwrt-v2ray.git package/lean/v2ray-core
 git clone -b luci2 https://github.com/kuoruan/luci-app-v2ray.git package/lean/luci-app-v2ray
