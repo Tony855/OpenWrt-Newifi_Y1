f28efd724d1271fab4bc99cf0ca74307b5b7b3c4
Tony855 <58850110+Tony855@users.noreply.github.com>
2020-10-26 03:19:03 +0700

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,6 +14,6 @@
 sed -i 's/192.168.1.1/192.168.28.1/g' package/base-files/files/bin/config_generate
 # sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-argon/root/etc/uci-defaults/30_luci-theme-argon
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
-git clone https://github.com/coolsnowwolf/lede/tree/master/package/lean /package/lean
-git clone https://github.com/kuoruan/openwrt-v2ray /package/lean/v2ray-core
-git clone -b luci2 https://github.com/kuoruan/luci-app-v2ray /package/lean/luci-app-v2ray
+git clone https://github.com/coolsnowwolf/lede/tree/master/package
+git clone https://github.com/kuoruan/openwrt-v2ray /package
+git clone -b luci2 https://github.com/kuoruan/luci-app-v2ray /package
