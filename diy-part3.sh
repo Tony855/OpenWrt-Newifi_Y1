f426f1544722ecd76fdd65b3a09dd3c512202b54
Tony855 <58850110+Tony855@users.noreply.github.com>
2020-10-26 21:44:23 +0700

    Add files via upload

---
+++ diy-part3.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+git clone https://github.com/coolsnowwolf/lede/tree/master/package/lean package/lean
+git clone https://github.com/kuoruan/openwrt-v2ray.git package/lean/v2ray-core
+git clone -b luci2 https://github.com/kuoruan/luci-app-v2ray.git package/lean/luci-app-v2ray
